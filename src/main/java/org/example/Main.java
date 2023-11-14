1a07253f1a9fb1377c804f160e1f9c3c96cab549
Ahmed Ali Khan <ahmedalikhan652@gmail.com>
2023-11-14 11:39:32 +0500

    Added a paragraph and attributes to the PDF.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,36 @@
 package org.example;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       Document document = new Document();
+       try {
+           PdfWriter writer = PdfWriter.getInstance(document,new FileOutputStream("HelloWorld.pdf"));
+           document.open();
+
+           //Method to add a paragraph.
+           document.add(new Paragraph("A Hello World Pdf Document. This document has some attribute attached to it."));
+
+           //Method to add attribute.
+           document.addAuthor("Ahmed Ali Khan");
+           document.addCreationDate();
+           document.addCreator("Ahmed.com");
+           document.addTitle("How to set attributes");
+           document.addSubject("This portion of the program show how to add attributes to a PDF.");
+
+           document.close();
+           writer.close();
+       } catch (DocumentException e ) {
+            e.printStackTrace();
+       } catch (FileNotFoundException e ) {
+           e.printStackTrace();
+       }
     }
 }
